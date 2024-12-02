81924c43426ac60c0fa6afc46cbb6806cf212f27
sharanpreet singh <sharanpreet1831.be21@chitkara.edu.in>
2024-12-02 11:17:30 +0530

    all detail about set

---
+++ Set.js
@@ -0,0 +1,26 @@
+                                                          /// SET 
+// Collection of unique data 
+// store any data but unique 
+
+let myarray =[1,2,3,4,4]
+let obj = new Set(myarray);
+console.log(obj);
+
+obj.add(5);
+console.log(obj);
+
+var obj1 = {name : "Sharan"};
+obj.add(obj1)
+console.log(obj)
+
+
+// for checking item is present or not in set 
+
+console.log(obj.has(1));
+console.log(obj.has(10));
+
+
+// for clear set ;
+obj.clear();
+console.log(obj)
+
